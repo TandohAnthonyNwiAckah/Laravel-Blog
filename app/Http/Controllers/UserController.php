10fdd6c1911e9a672476ac0a2a42fdf5e0779e37
Tandoh <anthony.tandoh@tanacom.io>
2020-07-26 20:49:18 +0000

    commit  Eloquent ORM CRUD

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,39 +6,76 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\DB;
 
+use App\User;
+
 class UserController extends Controller
 {
     //Create function called
 
     public function index()
     {
-    //return 'Hello World';
 
-     //Raw SQL QUERIES
 
-     //INSERT USERS
-     //  DB::insert('insert into users(name,email,password)
-     //  values(?,?,?)',['Tanacom','info@tanacom.io','password',]);
+     
+        //ELOQUENT ORM CRUD
+
+        //INSERT
+        // $user = new User();
+        // // dd($user);
+
+        // $user -> name = "Tandoh" ;
+        // $user -> email = "tandoh@tanacom.io";
+
+        // //use bcrypt to encrypt Password.
+        // $user ->  password = bcrypt('password');
+        // $user ->  save();
+
+
+
+
+        //UPDATE
+
+        User::where('id',4)-> update(['name' => 'Tanacom']);
+
+
+        // //SELECT
+        $user  = User::all();
+        return $user ;
+       
+        
+        //DELETE
+        // User::where('id',2)-> delete();
+
+
+
+
+
+         //return 'Hello World';
+
+        //Raw SQL QUERIES
+  
+         //INSERT USERS
+         //  DB::insert('insert into users(name,email,password)
+         //  values(?,?,?)',['Tanacom','info@tanacom.io','password',]);
 
  
-    //  // UPDATE USERS
+      //  // UPDATE USERS
 
-    //  DB::update('update users set name= ? where id = 1',['TONY']);
-
-
-
-    //DELETE USERS
-
-
-    //    DB::delete('delete from users where id = 1');
+      //  DB::update('update users set name= ? where id = 1',['TONY']);
 
 
 
-    //   // SELECT USERS
+       //DELETE USERS
 
-    //    $users =  DB::select('select * from users');
 
-    //   return $users;
+       //    DB::delete('delete from users where id = 1');
+
+
+       //   // SELECT USERS
+
+       //    $users =  DB::select('select * from users');
+
+       //   return $users;
 
 
        return view('home');
